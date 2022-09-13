d3498c2ce40b48145f73f221dc81ab659a4d3870
akhilagoskula <109618635+akhilagoskula@users.noreply.github.com>
2022-09-13 16:58:51 +0530

    Add files via upload

---
+++ biodata.py
@@ -0,0 +1,224 @@
+
+Firstname = input("Enter your firstname:")
+Lastname = input("Enter your lastname:")
+EmailID = input("Enter your EmailID:")
+Dateofbirth = input("Enter your dateofbirth:")
+MobileNo = int(input("Enter your mobileno:"))
+CareerObjective = input("Enter your careerobjective:")
+BTech = input("Enter your btech:")
+Diploma = input("Enter your diploma:")
+Schooling = input("Enter your schooling:")
+Strengths = input("Enter your strengths:")
+Hobbies = input("Enter your hobbies:")
+TechnicalSkills = input("Enter your technicalskills:")
+
+fstr1 = "First Name".ljust(20)
+fstr2 = "Last Name".ljust(20)        
+fstr3 = "EmailID".ljust(20)
+fstr4 = "Date of birth".ljust(20)
+fstr5 = "MobileNo".ljust(20)
+fstr6 = "CareerObjective".ljust(20)
+fstr7 = "BTech".ljust(20)
+fstr8 = "Diploma".ljust(20)
+fstr9 = "Schooling".ljust(20)
+fstr10 = "Strengths".ljust(20)
+fstr11 = "Hobbies".ljust(20)
+fstr12 = "TechnicalSkills".ljust(20)
+
+print("_____Bio Data_____")
+print(fstr1,":",Firstname)
+print(fstr2,":",Lastname
+print(fstr3,":",EmailID)
+print(fstr4,":",Dateofbirth)
+print(fstr5,":",MobileNo)
+print(fstr6,":",CareerObjective)
+print("_____Educational Qualification_____")
+print(fstr7,":",BTech)
+print(fstr8,":",Diploma)
+print(fstr9,":",schooling)
+print(fstr10,":",Strengths)
+print(fstr11,":",Hobbies)
+print(fstr12,":",TechnicalSkills)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
